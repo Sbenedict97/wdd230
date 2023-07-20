cc52daa4bfd9f9c10cdaef1c9f53faf859529921
Sbenedict97 <ben21002@byui.edu>
2023-07-19 19:41:19 -0600

    Updates to attribution

--- jv/finalproject/jv/script.js
+++ jv/finalproject/jv/script.js
@@ -1,25 +1,24 @@
-// Update the last modified date in the footer
+
 function updateLastModifiedDate() {
     const lastModifiedElement = document.getElementById("last-modified-date");
     const currentDate = new Date().toLocaleDateString("en-US");
     lastModifiedElement.textContent = currentDate;
   }
   
-  // Display the total number of submitted drinks
+ 
   function displaySubmittedDrinks() {
     const submittedDrinksElement = document.getElementById("submitted-drinks");
     const submittedDrinksCount = localStorage.getItem("submittedDrinksCount") || 0;
     submittedDrinksElement.textContent = submittedDrinksCount;
   }
   
-  // Update the total number of submitted drinks in local storage
   function updateSubmittedDrinksCount() {
     let submittedDrinksCount = localStorage.getItem("submittedDrinksCount") || 0;
     submittedDrinksCount++;
     localStorage.setItem("submittedDrinksCount", submittedDrinksCount);
   }
   
-  // Handle form submission and display the order details
+
   function handleFormSubmission(event) {
     event.preventDefault();
   
@@ -49,24 +48,21 @@ function updateLastModifiedDate() {
     const outputElement = document.getElementById("order-output");
     outputElement.innerHTML = formattedOutput;
   
-    // Update the total number of submitted drinks
     updateSubmittedDrinksCount();
   
-    // Display the updated count
+
     displaySubmittedDrinks();
   
     form.reset();
   }
-  
-  // Add event listeners when the DOM content is loaded
+
   document.addEventListener("DOMContentLoaded", () => {
-    // Update the last modified date in the footer
+
     updateLastModifiedDate();
   
-    // Display the total number of submitted drinks
     displaySubmittedDrinks();
   
-    // Handle form submission
+
     const form = document.getElementById("drink-form");
     form.addEventListener("submit", handleFormSubmission);
   });
--- jv/finalproject/jv/weather.js
+++ jv/finalproject/jv/weather.js
@@ -1,19 +1,17 @@
-// Replace 'YOUR_API_KEY' with your actual API key from OpenWeatherMap
-const apiKey = '5dd63cb22c8905d888a91caa77084d6e';
-const city = 'Carlsbad'; // Replace with your desired city
 
-// Function to fetch weather data from the API
+const apiKey = '5dd63cb22c8905d888a91caa77084d6e';
+const city = 'Carlsbad'; 
+
+
 async function getWeatherData() {
   try {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=$5334223&appid=$5dd63cb22c8905d888a91caa77084d6e`);
     const data = await response.json();
 
-    // Access the weather data from the response
     const temperature = data.main.temp;
     const condition = data.weather[0].description;
     const humidity = data.main.humidity;
 
-    // Update the DOM with the weather data
     document.getElementById('temperature').textContent = `${Math.round(temperature)}°F`;
     document.getElementById('condition').textContent = condition;
     document.getElementById('humidity').textContent = `${humidity}%`;
@@ -23,5 +21,5 @@ async function getWeatherData() {
   }
 }
 
-// Call the function to fetch weather data when the page loads
+
 window.addEventListener('load', getWeatherData);
